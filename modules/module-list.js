fb107c3c4ca7d1e289b383dc6259e644935a6d53
Gunnar Unger <gunnar.unger@gmail.com>
2023-01-06 17:28:52 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -10,6 +10,7 @@
                             ,form_module:"form-design-form",
                             design_panel:"entry-design-data",
                             sequence_panel:"sequence-form",
+                            preview:"preview-form",
                             task_name:"Form Design",
     };                   
     m[p+"form-design-form"] ={url:H+"/form-grid/form-design/form-design-form.html",Table:"form-design-form-builder",task_name:"Form Design Details"},
@@ -20,6 +21,7 @@
     m[p+"entry-design-form"]	={url:H+"/form-grid/form-design/entry-design-form.html",Table:"entry-design-form-builder",task_name:"Entry Design"}
 
     m[p+"sequence-form"]	={url:H+"/form-grid/form-design/sequence-form.html",Table:"entry-design-form-builder",task_name:"Sequencing Inputs"}
+    m[p+"preview-form"]	={url:H+"/form-grid/form-design/preview-form.html",Table:"entry-design-form-builder",task_name:"Preview Form"}
 
     //if(window.location.toString().indexOf('tb=demo')!=-1){
         for(p in $vm.module_list){
