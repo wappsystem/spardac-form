c3757e447ce48cf15f30524d0387892d8e6c9ad1
Gunnar Unger <gunnar.unger@gmail.com>
2023-09-29 16:49:29 +1000

    update

--- assets/js/form.01.js
+++ assets/js/form.01.js
@@ -9,7 +9,10 @@ m.load=function(){
     $('#F__ID')[0].reset();
     $('#submit__ID').show();
     $('#delete__ID').hide(); if(m.input!=undefined && m.input.record!=undefined && m.input.record._id!==undefined) $('#delete__ID').show();
-    if(m.input!=undefined && m.input.record!=undefined && m.input.record.Data!==undefined) $('#title__ID').text($vm.module_list[$vm.vm['__ID'].name].task_name+" - "+m.input.record.Data.Name);
+    if(m.input!=undefined && m.input.record!=undefined && m.input.record.Data!==undefined) {
+        $('#title__ID').text($vm.module_list[$vm.vm['__ID'].name].task_name+" - "+m.input.record.Data.Name);
+        $('#record__ID').val(m.input.record.UID)
+    }
     else $('#title__ID').text($vm.module_list[$vm.vm['__ID'].name].task_name);
     
     if(m.input.record!=undefined) {;
--- modules/module-list.js
+++ modules/module-list.js
@@ -14,6 +14,7 @@
                             task_name:"Form Design",
     };                   
     m[p+"form-design-form"] ={url:H+"/form-grid/form-design/form-design-form.html",Table:"form-design-form-builder",task_name:"Form Design Details"},
+
     m[p+"panel-main"]=    	        {url:H+"/panels/main.html",router:1},
     m[p+"panel-child"]=              {url:H+"/panels/child.html"},
 
@@ -24,6 +25,25 @@
     m[p+"preview-form"]	={url:H+"/form-grid/form-design/preview-form.html",Table:"entry-design-form-builder",task_name:"Preview Form",form_module:"preview"}
     //m[p+"preview"]	={url:H+"/form-grid/form-design/preview.html"}
     m[p+"preview"]	={url:H+"/previewer"}
+
+    m[p+"data-collection-configuration-data"] ={url:H+"/form-grid/data-collection-configuration/data-collection-configuration-data.html",Table:"data-collection-configuration-form-builder"
+                            ,form_module:"data-collection-configuration-form",
+                            participant:"participant-data",
+                            sequence_panel:"sequence-form",
+                            preview:"preview-form",
+                            task_name:"Data Collection Configuration",
+    };                   
+    m[p+"data-collection-configuration-form"] ={url:H+"/form-grid/data-collection-configuration/data-collection-configuration-form.html",Table:"data-collection-configuration-form-builder",task_name:"Data Collection Configuration"},
+    
+    m[p+"participant-data"]	={url:H+"/form-grid/form-design/participant-form-data.html",Table:"participant-form-builder",form_module:"participant-form",task_name:"Participant form",    
+        participant_entry_panel:"participant-entry-data",
+        participant_sequence_panel:"participant-sequence-form",
+        participant_preview:"participant-preview-form",
+        task_name:"Participant Form",
+    }
+    m[p+"participant-form"]	={url:H+"/form-grid/form-design/participant-form-form.html",Table:"participant-form-builder",task_name:"Participant form",form_module:"form-design-form"}
+    m[p+"participant-entry-data"]	={url:H+"/form-grid/form-design/participant-form-entry-data.html",Table:"participant-entry-form-builder",form_module:"participant-entry-form",task_name:"Participant form design"},
+    m[p+"participant-entry-form"]	={url:H+"/form-grid/form-design/participant-form-entry-form.html",Table:"participant-entry-form-builder",task_name:"Participant form design"}
 
     //if(window.location.toString().indexOf('tb=demo')!=-1){
         for(p in $vm.module_list){
